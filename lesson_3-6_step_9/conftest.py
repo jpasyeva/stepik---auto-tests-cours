90ece0ba15d6e5e1a2bf2e14294ba1c816fde432
Julia <jp@utair.digital>
2019-09-23 13:09:52 +0300

    Добавлено решение задачи

--- lesson_3-6_step_9/conftest.py
+++ lesson_3-6_step_9/conftest.py
@@ -1,1 +1,27 @@
-#Конфигурационный файл
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+# Добавление обработчика, который считывает из командной строки параметр language.
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language")
+
+# Логика запуска браузера с указанным языком пользователя.
+#Браузер объявляется в фикстуре browser и передается в тест как параметр.
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    browser = None
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    if language != None:
+        print("\nstart page..")
+        browser = webdriver.Chrome(options=options)
+
+    else:
+        raise pytest.UsageError("--don't choose language")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
--- lesson_3-6_step_9/test_items.py
+++ lesson_3-6_step_9/test_items.py
@@ -1,1 +1,10 @@
-#Тест для прохождения задания
+import time
+# Тест, который проверяет, что страница товара на сайте содержит кнопку добавления в корзину.
+
+def test_add_cart_button_on_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    #time.sleep(30)
+    browser.find_element_by_css_selector('.add-to-basket .btn')
+    check_text = browser.find_element_by_css_selector('.add-to-basket .btn').text
+    assert check_text != None
